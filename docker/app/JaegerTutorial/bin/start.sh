5eb75353d3c7e624870101fe3251b9cd85461735
Gowtham <ganesh.gowtham@gmail.com>
2020-04-23 00:28:47 +0530

    dummy commit

--- docker/app/JaegerTutorial/bin/start.sh
+++ docker/app/JaegerTutorial/bin/start.sh
@@ -5,4 +5,3 @@ export APP=com.github.ganeshgowtham.jaeger.BootstrapPaymentApplication
 export LOG_DIR=${APP_HOME}/logs
 export CLASSPATH=${CLASSPATH}:${LIBS}/*
 ${JAVA_HOME}/bin/java -enableassertions -cp ${CLASSPATH} ${LOGBACKCFG} ${APP}
-
